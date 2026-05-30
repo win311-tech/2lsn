ef06e5593ef91aa0535cbe13cb3379920c9bf4ef
Winfred <winfrednartey07@gmail.com>
2026-05-30 10:34:22 +0000

    Fix staticfiles dir for Render

--- Toolson/Toolson/settings.py
+++ Toolson/Toolson/settings.py
@@ -95,7 +95,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR.parent / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
